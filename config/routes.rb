2fc4be9ecaf535b1c612ed366c744efb559424a9
H4-M-masaki <MoriXrioM@gmail.com>
2022-04-15 06:49:17 +0000

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,12 @@
 Rails.application.routes.draw do
 
-
-  namespace :public do
-    get 'drives/new'
-    get 'drives/index'
-    get 'drives/edit'
-  end
   # 顧客用
-# URL /customers/sign_in ...
 devise_for :users,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
 
 # 管理者用
-# URL /admin/sign_in ...
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
@@ -24,18 +16,16 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   get '/about' => 'public/homes#about'
 
 
-    namespace :admin do
+  namespace :admin do
     root to: "homes#top"
     resources :users, only: [:index, :show, :edit, :updete]
-
   end
 
 
   namespace :public do
-
     resources :users, only: [:show, :edit, :updete, :create]
     resources :posts, only: [:index, :new, :show, :edit, :destroy, :create, :update]
-
+    resources :drives, only: [:new, :index, :edit, :updete, :destroy, :create]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
