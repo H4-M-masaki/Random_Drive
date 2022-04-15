983a6bbd381ec32c606071dba24bcc9fa7c16696
H4-M-masaki <MoriXrioM@gmail.com>
2022-04-15 09:20:44 +0000

    行きたい場所登録機能更新

--- app/controllers/public/drives_controller.rb
+++ app/controllers/public/drives_controller.rb
@@ -1,10 +1,41 @@
 class Public::DrivesController < ApplicationController
   def new
+    @drive = Drive.new
   end
 
   def index
+    @drives = Drive.all
   end
 
   def edit
+    @drive = Drive.find(params[:id])
+  end
+
+  def update
+      @drive = Drive.find(params[:id])
+   if @drive.update(drive_params)
+      redirect_to public_drives_path
+   end
+  end
+
+  def create
+    @drive = Drive.new(drive_params)
+    @drive.user_id = current_user.id
+    if @drive.save
+      redirect_to public_drives_path
+    end
+  end
+
+
+  def destroy
+  end
+
+
+
+private
+
+
+  def drive_params
+    params.require(:drive).permit(:address)
   end
 end
