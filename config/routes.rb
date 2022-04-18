275277d0f7f60495892c6e477df2e4475faf1f62
H4-M-masaki <MoriXrioM@gmail.com>
2022-04-18 13:32:47 +0000

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'posts/index'
+    get 'posts/show'
+  end
   # 顧客用
 devise_for :users,skip: [:passwords], controllers: {
   registrations: "public/registrations",
